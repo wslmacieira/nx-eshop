0a69276f35e667b89819af5e19ddf30dc3f0e6f8
wslmacieira <wslmacieira@outlook.com>
2021-06-28 11:58:56 -0300

    add categories form

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -7,6 +7,7 @@ import {CardModule} from 'primeng/card';
 import {ToolbarModule} from 'primeng/toolbar';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
+import {InputTextModule} from 'primeng/inputtext';
 
 import { CategoriesService } from '@dwll/products';
 
@@ -15,12 +16,14 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
 
 const UX_MODULE = [
   CardModule,
   ToolbarModule,
   ButtonModule,
-  TableModule
+  TableModule,
+  InputTextModule
 ]
 
 const routes: Routes = [
@@ -36,11 +39,15 @@ const routes: Routes = [
         path: 'categories',
         component: CategoriesListComponent
       },
+      {
+        path: 'categories/form',
+        component: CategoriesFormComponent
+      },
     ]
   }
 ]
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent],
+  declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent, CategoriesFormComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
