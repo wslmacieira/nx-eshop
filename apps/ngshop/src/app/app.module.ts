b8452068204e13d12339428ee46acd2edd2120a2
wslmacieira <wslmacieira@outlook.com>
2021-06-27 23:14:19 -0300

    creating components inside libraries

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from '@dwll/ui'
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
@@ -14,7 +15,7 @@ const routes: Routes = [
 ];
 @NgModule({
   declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
   providers: [],
   bootstrap: [AppComponent]
 })
