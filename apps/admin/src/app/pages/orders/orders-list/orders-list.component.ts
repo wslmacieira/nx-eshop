1e24a3004f68891d3beb09a44615e4dfa740a3fd
wslmacieira <wslmacieira@outlook.com>
2021-07-04 09:14:59 -0300

    order status

--- apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
+++ apps/admin/src/app/pages/orders/orders-list/orders-list.component.ts
@@ -1,6 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Order, OrdersService } from '@dwll/orders';
 
+interface IObjectKeys {
+  [key: string]: string | number | any;
+}
+const ORDER_STATUS: IObjectKeys = {
+  0: {
+    label: 'Pending',
+    color: 'primary'
+  },
+  1: {
+    label: 'Processed',
+    color: 'warning'
+  },
+  2: {
+    label: 'Shipped',
+    color: 'warning'
+  },
+  3: {
+    label: 'Delivered',
+    color: 'success'
+  },
+  4: {
+    label: 'Failed',
+    color: 'danger'
+  }
+}
+
 @Component({
   selector: 'admin-orders-list',
   templateUrl: './orders-list.component.html',
@@ -9,6 +35,7 @@ import { Order, OrdersService } from '@dwll/orders';
 })
 export class OrdersListComponent implements OnInit {
   orders: Order[] = [];
+  orderStatus = ORDER_STATUS;
 
   constructor(private ordersService: OrdersService) { }
 
