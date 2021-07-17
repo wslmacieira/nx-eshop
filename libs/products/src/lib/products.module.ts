a8025182c0aea739c956d41747bb68421e712712
wslmacieira <wslmacieira@outlook.com>
2021-07-17 07:58:33 -0300

    product item

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,16 +1,16 @@
+import { ButtonModule } from 'primeng/button';
 import { OrdersModule } from './../../../orders/src/lib/orders.module';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsSearchComponent } from './components/products-search/products-search.component';
 import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 
 @NgModule({
-  imports: [CommonModule, OrdersModule, RouterModule],
-  declarations: [
-    ProductsSearchComponent,
-    CategoriesBannerComponent
-  ],
-  exports: [ProductsSearchComponent, CategoriesBannerComponent]
+  imports: [CommonModule, OrdersModule, RouterModule, ButtonModule],
+  declarations: [ProductsSearchComponent, CategoriesBannerComponent, ProductItemComponent, FeaturedProductsComponent],
+  exports: [ProductsSearchComponent, CategoriesBannerComponent, ProductItemComponent, FeaturedProductsComponent]
 })
 export class ProductsModule {}
