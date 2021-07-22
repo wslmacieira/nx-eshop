3b5d9d88ea69d9e92367fee8bd9594fb0b513c43
wslmacieira <wslmacieira@outlook.com>
2021-07-22 16:36:19 -0300

    add cart confirmation toast

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,23 +1,34 @@
-import { ProductsModule } from '@dwll/products';
-import { ProductsSearchComponent } from './../../../../libs/products/src/lib/components/products-search/products-search.component';
-import { UiModule } from '@dwll/ui';
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ProductsModule } from '@dwll/products';
+import { UiModule } from '@dwll/ui';
+import { OrdersModule } from '@dwll/orders';
+
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+import { AccordionModule } from 'primeng/accordion';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { AccordionModule } from 'primeng/accordion';
 import { NavComponent } from './shared/nav/nav.component';
-import { HttpClientModule } from '@angular/common/http';
-import { OrdersModule } from '@dwll/orders';
+import { MessagesComponent } from './shared/messages/messages.component';
 
 const routes: Routes = [{ path: '', component: HomePageComponent }];
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, HeaderComponent, FooterComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    MessagesComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -26,9 +37,10 @@ const routes: Routes = [{ path: '', component: HomePageComponent }];
     ProductsModule,
     AccordionModule,
     UiModule,
-    OrdersModule
+    OrdersModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- apps/ngshop/src/app/shared/messages/messages.component.ts
+++ apps/ngshop/src/app/shared/messages/messages.component.ts
@@ -1,20 +1,21 @@
-import { CartService } from '@dwll/orders';
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '@dwll/orders';
 import { MessageService } from 'primeng/api';
 
 @Component({
-  selector: 'ngshop-header',
-  templateUrl: './header.component.html'
+  selector: 'ngshop-messages',
+  templateUrl: './messages.component.html',
+  styles: []
 })
-export class HeaderComponent implements OnInit {
+export class MessagesComponent implements OnInit {
   constructor(private cartService: CartService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.cartService.cart$.subscribe(() => {
       this.messageService.add({
         severity: 'success',
-        summary: 'success',
-        detail: 'Cart added!'
+        summary: 'Success',
+        detail: 'Cart Updated!'
       });
     });
   }
