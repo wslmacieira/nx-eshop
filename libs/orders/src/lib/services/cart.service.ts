d218eda1d78d4949abb2699bb1f9d818078bb5fc
wslmacieira <wslmacieira@outlook.com>
2021-07-22 15:31:39 -0300

    count badge in the header component

--- libs/orders/src/lib/services/cart.service.ts
+++ libs/orders/src/lib/services/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Cart, CartItem } from '../models/cart.model';
 
 export const CART_KEY = 'cart';
@@ -7,6 +8,7 @@ export const CART_KEY = 'cart';
   providedIn: 'root'
 })
 export class CartService {
+  cart$: BehaviorSubject<Cart> = new BehaviorSubject(this.getCart());
   constructor() {}
 
   initCartLocalStorage() {
@@ -17,13 +19,14 @@ export class CartService {
       };
       const initialCartJson = JSON.stringify(initialCart);
       localStorage.setItem(CART_KEY, initialCartJson);
+    } else {
+      this.cart$.next(cart);
     }
-
   }
 
   getCart(): Cart {
-    const cartJsonString: string = localStorage.getItem(CART_KEY) ?? '';
-    const cart: Cart = JSON.parse(cartJsonString);
+    const cartJsonString = localStorage.getItem(CART_KEY!);
+    const cart: Cart = JSON.parse(cartJsonString!);
     return cart;
   }
 
@@ -31,13 +34,14 @@ export class CartService {
     const cart = this.getCart();
     const cartItemExist = cart.items?.find((item) => item.productId === cartItem.productId);
     if (cartItemExist) {
-      cartItemExist.quantity = (cartItemExist.quantity ?? 0) + (cartItem.quantity ?? 0);
+      cartItemExist.quantity = cartItemExist.quantity! + cartItem.quantity!;
     } else {
       cart.items?.push(cartItem);
     }
     const cartJson = JSON.stringify(cart);
 
     localStorage.setItem(CART_KEY, cartJson);
+    this.cart$.next(cart);
     return cart;
   }
 }
